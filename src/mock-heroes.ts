d6de09fcb8bb99f312ee5af2294c3bc5c98a8b46
Andrea <erricoandrea93@gmail.com>
2019-11-30 18:59:48 +0100

    display list

---
+++ src/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Iron Man'},
+  {id: 12, name: 'Captain America'},
+  {id: 13, name: 'Hulk'},
+  {id: 14, name: 'Thor'},
+  {id: 15, name: 'Black Widow'},
+  {id: 16, name: 'Batman'},
+  {id: 17, name: 'Superman'},
+  {id: 18, name: 'Robin'}
+];
